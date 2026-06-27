3be5dff0b8e7547a3eecbda5d80d26314788d3d5
Thomas Kilian <thomas-kilian@gmx.net>
2026-06-27 13:35:14 +0200

    More changes

--- home/modules/sway/keybinds/default.nix
+++ home/modules/sway/keybinds/default.nix
@@ -35,7 +35,7 @@
   powerMenu = pkgs.writeShellScriptBin "powerMenu.sh" ''
     #!/usr/bin/env bash
 
-    op=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | ${pkgs.rofi-wayland}/bin/rofi -i -dmenu | ${pkgs.gawk}/bin/awk '{print tolower($2)}')
+    op=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | ${pkgs.rofi}/bin/rofi -i -dmenu | ${pkgs.gawk}/bin/awk '{print tolower($2)}')
 
     case $op in
       poweroff)
